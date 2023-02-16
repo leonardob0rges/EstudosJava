7030cb5a77ad16e9684a95aae1d3826581a62498
Leonardo Borges <leonardoaborges13@gmail.com>
2023-02-16 11:07:34 -0300

    this is java

---
+++ HelloWorld.java
@@ -0,0 +1,32 @@
+class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
+/* criação da classe (com o mesmo nome do arquivo)*/
+/* toda aplicação java o ponto de "start" da aplicação é o 
+método: public static void main(String[] args){}
+método main*/
+/* o comando System.out.println("Hello world Java!") 
+imprime a mensagem(string no console do 
+usuário)
+*/
+
+/*
+- o java é uma linguagem interpretada, diferente do JS
+que é interpretada;
+- criação de uma classe;
+- salvar o arquivo em .java, com o mesmo nome da classe 
+criada. posteriormente, ser compilado com o java 
+compiler (javac);
+- ocorre a geração de bytecode (código gerado após a 
+compilação), e é executado pela JVM (Java Virtual 
+Machine);
+- o javac.exe (compilador) deve ser usado para compilar
+o programa, passando como comando: 
+javac + nomedoarquivo.java, com a extensão, para não
+ocorrer erro na execução;
+- o java é executado passando só o nome da classe
+(código compilado) sem a extensão .class, porque o 
+java.exe só trabalha com arquivos .class;
+*/
