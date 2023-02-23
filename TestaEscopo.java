060dea49fda5dbe1dc21b3eca9454c9cf934f7b9
Leonardo Borges <leonardoaborges13@gmail.com>
2023-02-23 12:15:01 -0300

    scope

--- TestaEscopo.java
+++ TestaEscopo.java
@@ -32,6 +32,48 @@ public class TestaEscopo {
             System.out.println("infelizmente você não pode entrar");
         }
 
+        boolean foiPromovido = true;
+        double salario;
+
+        if(foiPromovido) {
+            salario = 4200.0;
+        } else {
+            salario = 3800.0;
+        }
+
+        System.out.println(salario);
+
 
     }
 }
+
+/*
+Escopo:
+
+Declaração da variável
+Dessa vez, João fez uma implementação para calcular o salário de um funcionário em caso de promoção.
+Ele fez a seguinte implementação:
+
+public class TesteSalario {
+
+    public static void main(String[] args) {
+
+        boolean foiPromovido = true;
+
+        if(foiPromovido) {
+            double salario = 4200.0;
+        } else {
+            double salario = 3800.0;
+        }
+
+        System.out.println(salario);
+    }
+}
+
+Qual será o resultado da compilação/execução?
+
+O código nem compila, pois fora do bloco if/else a variável salario não existe mais.
+
+Correto, a variável salario só é visível dentro do bloco if/else.
+
+ */
